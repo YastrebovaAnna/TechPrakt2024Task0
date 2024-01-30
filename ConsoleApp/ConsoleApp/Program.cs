92ee87d2d53c34fb1904ea556d48a7546f6f4874
YastrebovaAnna <ipz221_yaav@student.ztu.edu.ua>
2024-01-30 20:24:42 +0200

    Created .gitignore and console project

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter the first number: ");
+        if (double.TryParse(Console.ReadLine(), out double number1))
+        {
+            Console.Write("Enter the second number: ");
+            if (double.TryParse(Console.ReadLine(), out double number2))
+                Console.WriteLine($"{number1} + {number2} = {number1 + number2}");
+            else
+                Console.WriteLine("Incorrect second number format.");
+        }
+        else
+            Console.WriteLine("First number format is incorrect.");
+    }
+}
